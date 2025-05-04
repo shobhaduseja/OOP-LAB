d87fbb9738c02fe52520354109dd87201d024a11
shobhaduseja <shobuduseja13@gmail.com>
2025-05-04 15:50:08 +0500

    Create task5.cpp

---
+++ Lab 10/task5.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main(){
+    ifstream in;
+    in.open("vehicles.txt");
+    if(!in.is_open()){
+        cerr<<"Error: File could not be opened!"<<endl;
+        return 1;
+    }
+
+    string line;
+
+    while(getline(in, line)){
+        istringstream s(line);
+        string Type, ID, Name, Year, ExtraData, Certification;
+
+        getline(s, Type, ',');
+        getline(s, ID, ',');
+        getline(s, Name, ',');
+        getline(s, Year, ',');
+        getline(s, ExtraData, ',');
+        getline(s, Certification, ',');
+
+        if(Type=="AutonomousCar"){
+            size_t pos= ExtraData.find(":");
+            if(pos!= string::npos){
+                string str = ExtraData.substr(pos+1);
+                float SoftwareVersion = stof(str);
+                cout<<"AutonomousCar ID: "<<ID<<"\nSoftware Version: "<<SoftwareVersion<<endl;
+            }   
+        }
+        else if(Type=="ElectricVehicle"){
+            size_t pos=ExtraData.find(":");
+            if(pos!=string::npos){
+                string str = ExtraData.substr(pos+1);
+                int BatteryCapacity = stoi(str);
+                cout<<"ElectricVehicle ID: "<<ID<<"\nBattery Capacity: "<<BatteryCapacity<<endl;
+            }
+        }
+        else if(Type=="HybridTruck"){
+            size_t pos = ExtraData.find(":");
+            size_t pos2 = ExtraData.find("|");
+            if(pos!=string::npos && pos2!=string::npos){
+                string str=ExtraData.substr(pos+1, pos2-1);
+                int CargoValue = stoi(str);
+                string str2= ExtraData.substr(pos2+1);
+                int BatteryCapacity = stoi(str);
+                cout<<"HybridTruck ID: "<<ID<<"\nCardo Value: "<<CargoValue<<"\nBattery Capacity: "<<BatteryCapacity<<endl;
+            }
+        }
+        else{
+            cout << "Unknown vehicle type! "<< endl;
+        }
+    }
+
+    in.close();
+    return 0;
+}
